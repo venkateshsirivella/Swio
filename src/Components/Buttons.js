af239ae8d080145fc9c3475dab07a7d1b3b35951
venkatesh sirivella <sirivellavenkatesh636@gmail.com>
2024-05-27 10:53:29 +0530

    icons updated

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Components/Buttons.css';
-import icon from '../Components/Images/Playstore.jpeg'
+import icon from '../Components/Images/applelogo.png'
 import { Link } from 'react-router-dom';
 import shelf from '../Components/Images/ShelfCallout.png';
 
@@ -8,7 +8,7 @@ function Buttons() {
     return (
         <div className='mainpart'>
             <div className='buttonSection'>
-                <a className='linkbtn' href='https://play.google.com/store/apps/details?id=com.spotify.music&pli=1'><button className='storebtn'><div><img className='icon' src={icon} alt='' /></div><div>Google PlayStore</div></button></a>
+                <a className='linkbtn' href='https://apps.apple.com/us/app/sleeve-for-spotify-music/id1606145041?mt=12'><button className='storebtn'><div><img className='icon' src={icon} alt='' /></div><div>Mac Store</div></button></a>
                 <Link to='/Payment'><button className='buyButton'><div>Buy Directly</div><div>$5.99</div></button></Link>
             </div>
             <div className='subscriptionMsg'>
